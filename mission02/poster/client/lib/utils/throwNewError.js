041d49b9124e89b101f25ad6ecb83354a651b51e
ParkjiDev <parkjidev@gmail.com>
2023-12-16 23:07:02 +0900

    [feat] 유틸 함수 추가 - throwNewError.js 추가 - typeOf.js 추가

---
+++ mission02/poster/client/lib/utils/throwNewError.js
@@ -0,0 +1,3 @@
+function throwTypeError(message) {
+  throw new TypeError(message);
+}
---
+++ mission02/poster/client/lib/utils/typeOf.js
@@ -0,0 +1,6 @@
+function isString(data) {
+  return (
+    Object.prototype.toString.call(data).slice(8, -1).toLocaleLowerCase() ===
+    "string"
+  );
+}
