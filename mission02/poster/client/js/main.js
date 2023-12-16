732ce0ef3d7ddc24f4590bdc4e86e33c5ae96757
ParkjiDev <parkjidev@gmail.com>
2023-12-16 22:41:21 +0900

    [feat] 기능 구현 - 이미지 선택시 class 추가 - 이미지 선택시 배경색 변경 - 이미지 선택시 visual Img 변경 - 이미지 선택시 h1 text 변경

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,16 +8,52 @@
 
 */
 
+const body = getNode("body");
+const h1 = getNode(".nickName");
+const visualImg = getNode(".visual img");
+const navigation = getNode(".nav");
+const list = getNodes(".nav li");
 
+bindEvent(navigation, "click", handleClick);
 
+function handleClick(e) {
+  const li = e.target.closest("li");
+  if (!li) return;
 
+  selectImg(li);
+  setBgColor(li);
+  setImage(li);
+  setNameText(li);
+}
 
+function selectImg(li) {
+  list.forEach((element) => {
+    removeClass(element, "is-active");
+  });
+  addClass(li, "is-active");
+}
 
+function setBgColor(li) {
+  const index = li.dataset.index;
+  const bgColorA = data[index - 1].color[0];
+  const bgColorB = data[index - 1].color[1];
+  if (!bgColorB) bgColorB = "#000";
 
+  body.style.background = `linear-gradient(to bottom, ${bgColorA}, ${bgColorB})`;
+}
 
+function setImage(li) {
+  const index = li.dataset.index;
 
+  const imgSrc = `./assets/${data[index - 1].name.toLowerCase()}.jpeg`;
+  const alt = data[index - 1].alt;
 
+  attr(visualImg, "src", imgSrc);
+  attr(visualImg, "alt", alt);
+}
 
-
-
-
+function setNameText(li) {
+  const index = li.dataset.index;
+  const nameText = data[index - 1].name;
+  h1.textContent = nameText;
+}
