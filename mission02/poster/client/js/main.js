422235beff88220914385209e9f3be80fce4ec68
ParkjiDev <parkjidev@gmail.com>
2023-12-17 11:07:17 +0900

    [comment] 주석 추가

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -14,19 +14,21 @@ const visualImg = getNode(".visual img");
 const navigation = getNode(".nav");
 const list = getNodes(".nav li");
 
+// 이미지 클릭에 대한 eventListener 등록
 bindEvent(navigation, "click", handleClick);
 
 function handleClick(e) {
   const li = e.target.closest("li");
   if (!li) return;
 
-  selectImg(li);
-  setBgColor(li);
-  setImage(li);
-  setNameText(li);
-  setAudio(li);
+  selectImg(li); // 이미지 선택시 흰색 테두리 박스 처리
+  setBgColor(li); // 선택한 이미지에 따라 배경색 변경
+  setImage(li); // visual img 변경
+  setNameText(li); // 선택한 이미지에 따라 h1 변경
+  setAudio(li); // 선택한 이미지에 따라 오디오 파일 재생
 }
 
+// 이미지 선택시 흰색 테두리 박스 처리
 function selectImg(li) {
   list.forEach((element) => {
     removeClass(element, "is-active");
@@ -34,6 +36,7 @@ function selectImg(li) {
   addClass(li, "is-active");
 }
 
+// 선택한 이미지에 따라 배경색 변경
 function setBgColor(li) {
   const index = li.dataset.index;
   const bgColorA = data[index - 1].color[0];
@@ -43,6 +46,7 @@ function setBgColor(li) {
   body.style.background = `linear-gradient(to bottom, ${bgColorA}, ${bgColorB})`;
 }
 
+// visual img 변경
 function setImage(li) {
   const index = li.dataset.index;
   const imgSrc = `./assets/${data[index - 1].name.toLowerCase()}.jpeg`;
@@ -52,12 +56,14 @@ function setImage(li) {
   attr(visualImg, "alt", alt);
 }
 
+// 선택한 이미지에 따라 h1 변경
 function setNameText(li) {
   const index = li.dataset.index;
   const nameText = data[index - 1].name;
   h1.textContent = nameText;
 }
 
+// 선택한 이미지에 따라 오디오 파일 재생
 function setAudio(li) {
   const audioSrc = selectAudio(li);
   const audio = new AudioPlayer(audioSrc);
